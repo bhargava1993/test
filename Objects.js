225ac601402b6dee30c7f949b0c3123645806fd5
Venu <muddangulavenukumar@gmail.com>
2025-11-11 20:05:58 +0530

    Creating arrays,objects and operators

---
+++ Objects.js
@@ -0,0 +1,33 @@
+var family = { father: "Rama", mother: "Seetha", grandfather: "janaka Maharaj", my_name: "lavakusha" };
+console.log("line--->2", family);
+
+
+
+var fruits_price = {
+    apple: 100,
+    pine_apple: 120,
+    banana: 40,
+    grapes: 80
+};
+console.log("line--->12", fruits_price);
+
+
+var object = { name: "Venukumar", age: 21, city: "Bangalore" };
+console.log("line--->16", object);
+
+
+
+var salary = { Venu: 40000.56, Jagan: 37000.00, Anji: 35000.0 };
+console.log("line--->21", salary);
+
+
+
+var animal_colour = { Tiger: "Yellow", dog: "Black", cat: "white", Lion: "Yellow" };
+console.log("line--->26", animal_colour);
+
+
+var class_students = { class_1: 50, class_2: 40, class_3: 28, class_4: 60 };
+console.log("line--->30", class_students);
+
+var marks = { maths: 74, social: 70, english: 60, Telugu: 80 };
+console.log("line--->33", marks);
