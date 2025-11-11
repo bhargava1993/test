f3ea8f739473ea8d807449e35c13b3397eafba46
Sudheer <balagundlanswamy123@gmail.com>
2025-11-11 07:49:25 +0530

    Data Types of JavaScript

---
+++ datatypes.js
@@ -0,0 +1,135 @@
+console.log("JAVA SCRIPT DATA TYPES");
+
+
+// Create 10 arrays 
+
+/*
+var jobs = ["Developer","Testing","Automation","Customer Support"]
+console.log("Array ---1:",jobs);
+
+var names = ["Sudheer","Sam","Jamun",7683,5491,5.23,'s']
+console.log("Array --2",names);
+
+var bikes = ["KTM","Duke","Royal Enfield","NS Pulsar","MT 15"]
+console.log("Array --3: ", bikes);
+
+var cars = ["Maruthi Suzuki", "Toyota", "TATA", "Mahindra","Hyundai"]
+console.log("Array-- 4:", cars);
+
+var cities = ["Bengaluru", "Hyderabad", "Chennai", "Kolkata","Delhi"]
+console.log("Array--5: ", cities);
+
+var tokens = ["ss5",145,2.75,"K56","sos5"]
+console.log("Array--6",tokens);
+
+var collections = ["Sarees","Pants",true,false,"123",123]
+console.log("Array --7: ", collections);
+
+var fuels = ["Petrol","Diesel","CNG","Helium Gas","Hydrogen Gas"]
+console.log("Array --8: ", fuels);
+
+var system = ["Lenovo","HP","Mac","Dell","Samsung"]
+console.log("Array --9: ", system);
+
+var fruits = ["Pomergranate","Apple","Custard Apple","Mango"]
+console.log("Array --10: ", fruits);
+*/
+
+
+
+// Create 10 Objects
+
+var dailyroutine = {
+    face:"Mucchstac",
+    Hair:"Loreal",
+    Body:"Dove Soap",
+    washing: "Surf Excel"
+}
+console.log("Object--1: ", dailyroutine);
+
+var OS = {
+    Motorola: "Android",
+    Iphone: "IOS",
+    LenovoLaptop:"Windows",
+    Mac:"IOS"
+}
+console.log("Object--2: ", OS);
+
+var tastes = {
+    salt:"Salt",
+    Sugar:"Sweet",
+    bittergourd:"Bitter",
+    Tea:"Hot",
+    water:"Cold"
+}
+
+console.log("Object--3", tastes);
+
+
+var vehicles = {
+    car:"Diesel",
+    bikes:"Petrol",
+    Ev:"Electric",
+    Auto:"CNG",
+    Aeroplanes:"Helium Gas"
+}
+
+console.log("Object--4: ",vehicles);
+
+
+var capitals = {
+    AndhraPradesh: "Amaravathi",
+    Telangana: "Hyderabad",
+    Tamilnadu: "Chennai",
+    Karnataka: "Bengaluru",
+    Kerala: "Thiruvananthapuramu"
+}
+
+console.log("Object--5: ",capitals);
+
+var aircrafts = {
+    India: "Tejas",
+    Russia: "Sukhoi",
+    USA: "F22 Raptor",
+    EUnation: "Typhoon",
+    China: "J20 Mighty Dragon" 
+}
+
+console.log("Object--6: ", aircrafts);
+
+var SocialMedia = {
+    youtube: "Entertainment Platform",
+    snapchat: "Camera",
+    messenger: "Chat",
+    omegle: "Chit Chat",
+    anmazon: "E-commerce"
+}
+console.log("Object--7: ", SocialMedia);
+
+var furniture = {
+    table: "Wooden",
+    cup: "Glass",
+    plate: "Steel",
+    rod: "Iron",
+    shirt: "Thread"
+}
+console.log("Object--8: ", furniture);
+
+var Heroes = {
+    Alluarjun : "Stylish Star",
+    Mahesh: "Prince",
+    NTR: "Young Tiger",
+    RamCharan: "Mega Power Star",
+    prabhas: "Rebel Star"
+}
+
+console.log("Object--9: ", Heroes);
+
+var languages= {
+    python: "High Level Language",
+    java: "Popular Language",
+    javascript: "Scripting Language",
+    c: "Mid Level Language"
+}
+
+console.log("Object--10: ", languages);
---
+++ practiceDataTypes.js
@@ -0,0 +1,48 @@
+// Example of Number Data Type
+
+var  passwrd = 2430;
+console.log(passwrd);
+console.log(typeof passwrd);
+
+// Example of String Data Type
+
+var collegeName= "Sathyabama Institute of Science and Technology";
+console.log(collegeName);
+console.log(typeof collegeName);
+
+// Example of null Data Type
+
+var dist = null
+console.log(dist);
+console.log(typeof dist);
+
+// Example of Boolean Data Type
+
+var stat = true;
+console.log(stat);
+console.log(typeof stat);
+
+// Example of BigInt
+
+var BigValue = 123455823928732826;
+console.log(BigValue);
+console.log(typeof BigValue);
+
+// Example of Array Data Type
+
+var Collect = [1,'s',"Sudheer",672];
+console.log(Collect);
+console.log(typeof Collect);
+
+// Example of Object Data Type
+
+var obj = {
+    ph_num : 12345,
+    name: "Sid",
+    fruit: "Guava",
+    vegetable: "Tomato",
+    jaggery: "Sweet",
+}
+
+console.log(obj);
+console.log(typeof obj);
