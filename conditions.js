3fd453658397e113c330465b9d3cc947829b3f3b
unknown <1993bhargava@gmail.com>
2025-11-17 08:56:09 +0530

    loops

--- conditions.js
+++ conditions.js
@@ -64,12 +64,117 @@ months
 voter
 
 
+// Nested if–else: Checking Login Role
+
+// Age + Citizenship Check
+
+let age = 21;
+let citizen = true;
+
+if (age >= 21) {
+    
+    if (citizen) {
+        console.log("You are eligible to vote.");
+    } else {
+        console.log("You must be a citizen to vote.");
+    }
+} else {
+    console.log("You are too young to vote.");
+}
+
+
+// Checking Login Role
+
+let username = "admin";
+let password = "1234";
+
+if (username === "admin") {
+    if (password === "1234") {
+        console.log("Login successful. Welcome, Admin!");
+    } else {
+        console.log("Incorrect password.");
+    }
+} else {
+    console.log("Username not found.");
+}
+
+//Grade Calculator
+
+let score = 85;
+
+if (score >= 0 && score <= 100) {
+    if (score >= 90) {
+        console.log("Grade: A");
+    } else if (score >= 80) {
+        console.log("Grade: B");
+    } else if (score >= 70) {
+        console.log("Grade: C");
+    } else {
+        console.log("Grade: D or F");
+    }
+} else {
+    console.log("Invalid score!");
+}
+
+// Number is Positive, Negative or Zero — and Even/Odd
+let num = 7;
+
+if (num > 0) {
+    if (num % 2 === 0) {
+        console.log("Positive and even");
+    } else {
+        console.log("Positive and odd");
+    }
+} else if (num < 0) {
+    console.log("Negative number");
+} else {
+    console.log("Zero");
+}
+
+// ATM Withdrawal Logic
+let balance = 500;
+let withdraw = 200;
+
+if (withdraw > 0) {
+    if (withdraw <= balance) {
+        console.log("Withdrawal successful!");
+    } else {
+        console.log("Insufficient balance.");
+    }
+} else {
+    console.log("Invalid amount!");
+}
+
+
+// Temperature Check (Nested Conditions)
+
+let temp = 32;
+
+if (temp >= 0) {
+    if (temp <= 30) {
+        console.log("Weather is normal");
+    } else {
+        console.log("It's hot outside");
+    }
+} else {
+    console.log("It's freezing!");
+}
+
+//  Shopping Discount
+let amount = 120;
+
+if (amount >= 50) {
+    if (amount >= 100) {
+        console.log("You get a 20% discount!");
+    } else {
+        console.log("You get a 10% discount!");
+    }
+} else {
+    console.log("No discount.");
+}
 
 
 
 
-
-
-// looping statement
 
 
---
+++ loops.js
@@ -0,0 +1,132 @@
+// looping statement
+
+// ++
+// --
+
+// ++
+// 6
+// a++
+
+// 6+1= 7
+// 7+1 =8
+// 8+1= 9
+// 9+1=10
+
+// --
+// 6
+// 6-1 = 5
+// 5-1= 4
+// 4-1 =3
+// 3-1 =2
+// 2-1 = 1
+
+// syntax:
+// ---------
+
+// for (Declaration/initilization;condition;increment/decrement){
+//     logic
+// }
+
+// for(let i=0;i<5 ; i++){
+// logic
+// }
+
+// console.log(i<10)
+// 0,1,2,3,4,5,6,7,8,9,    10
+
+// 0+1 =1
+
+
+
+
+
+// console.log(0)
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+
+// console.log(1000)
+
+// starting  -->  1000
+// 100      to   1000
+
+// let i=100;
+// let finalOutput= 10000;
+
+// let condtion = i < finalOutput;
+
+// 2,4,6,8
+
+// i+=2;
+
+// i%2==0
+
+// 1,3,5,7,9
+
+// 10/2 = 0
+// 9/2 = 1
+
+// 2)10 (5
+//    10
+//  ------------  
+//     0
+
+//  2) 9 (4   
+//     8
+//    ------
+//     1
+
+
+// 10/2 =5 
+
+// console.log(10/2);
+// console.log(10%2);
+
+// for(let i=1; i < 10;i++){
+    
+//    if(i%2 === 2){
+//     // console.log(i);
+//    }
+//    else{
+//     console.log(i);
+//    }
+    
+// }
+
+// let array = ["bhargava", "venu", "sudheer", "zaiba"];
+                // 0           1       2           3
+                    // 0       0+1         1+1    2+1
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+
+// for(let i = 0; i<4 ;i++){
+//     console.log(array[i])
+// }
+
+// 10,9,8,7,6,5,4,3,2,1,0
+
+// -- 
+// 10-1 =9
+// 9-1 =8
+
+// 10 ---> 0
+// let array = "121";
+
+// for(let i=3; i >= 0 ;i--){
+//     console.log(array[i]);
+// }
+
+// 121
+
+
+1 - 100
+100 - 1
+
