bfc7660a758ac003f571901f040c568f4b9776ed
Sudheerkumar <balagundlanswamy123@gmail.com>
2025-11-17 21:18:25 +0530

    Operators

---
+++ practiceOperators.js
@@ -0,0 +1,207 @@
+/* Arithmetic Operators  */
+
+// console.log("Arithmetic Operators: --> Addition");
+
+// var a = 10;
+// var b = 20;
+// var c = "30";
+// var result = a + b;
+// console.log(result);
+// console.log(a+c);
+
+// var x = 10;
+// var y = 30;
+// var a;
+// console.log((x+y) + a);
+
+// OUTPUT IS NOT A NUMBER(NaN)
+
+
+
+// Subtraction
+
+// var a = 22;
+// var b = 31;
+// var c = 41;
+// console.log(a-b);
+// console.log(a-b-c);
+
+
+// Multiplication
+
+// var a = 54;
+// var b = 34;
+// var c = 12;
+// var d = 122;
+
+// console.log(a*b);
+// console.log(b*c);
+// console.log(c*d);
+// console.log(a*b*c);
+// console.log(b*c*d);
+// console.log(a*b*c*d);
+
+
+// Division
+
+// var a = 4;
+// var b = 2;
+// console.log(a/b);
+// console.log("Sam"/2);
+
+// Modulus --> returns remainder
+
+// var a =122;
+// var b = 33;
+
+// console.log(a%b);
+
+
+// Power **
+
+// console.log(2**4);
+
+// var a = 6;
+// var b = 4;
+// console.log(a**b);
+
+// Increment and Decrement Operators
+
+// Increment Operator
+
+
+// var a = 10;
+// a++;
+// console.log(a);
+// a++;
+// console.log(a);
+
+
+// Decrement Operators
+
+// var b = 44;
+// console.log(b);
+// b--;
+// console.log(b);
+// console.log(--b);
+// console.log(++b);
+
+// var a = 12;
+// var b = ++a;
+// var c = --a;
+// var d = ++a;
+// var e = --a;
+// var f = e++;
+// console.log("a = ",a);
+// console.log("b = ",b);
+// console.log("c = ",c);
+// console.log("d = ",d);
+// console.log("e = ",e);
+// console.log("f = ",f);
+
+//  POST INCREMENT
+// var a = 10;
+// var c = a++;
+// console.log(c);
+
+
+// POST DECREMENT
+
+// var b = 2;
+// var d = b--;
+// console.log(d);
+
+// ASSIGNMENT OPERATORS
+
+// Assign Operator "="
+
+// var a = 10;
+// console.log(a);
+
+// EQUAL OPERATORS
+
+// var a = 10;
+// var b = 12;
+// var c = 10;
+// var d = "12";
+
+// console.log(a==b);
+// console.log(a==c);
+// console.log(b==d);
+
+// STRICT EQUAL
+
+// var s = 10;
+// var d = "10";
+// var e = 10;
+// var f = 12;
+
+// console.log(s===e);
+// console.log(s===d);
+// console.log(e===f);
+
+
+// Lesser Than Operator
+
+// var a = 10;
+// var b = 20;
+
+// console.log(a<b);
+
+// var d = 12;
+// var e = 2;
+
+// console.log(d<e);
+
+
+// Greater than
+
+// var a = 1;
+// var b = 14;
+
+// console.log(a>b);
+
+// var e = 45;
+// var f = 10;
+
+// console.log(e>f);
+
+// Lesser Than
+
+// var a = 10;
+// var b = 20;
+// var c = 23;
+// var d = 10;
+
+// console.log(a<=b);
+// console.log(a<=d);
+// console.log(c<=b);
+
+// Greater Than 
+
+// var a = 10;
+// var b = 19;
+// var c = 19;
+// var d = 1;
+
+// console.log(a>=b);
+// console.log(a>=c);
+// console.log(a>=d);
+
+// Not Equal != & !==
+
+// var a = 12;
+// var b = 10;
+// var c = 12;
+
+// console.log(a!=b);
+// console.log(a!=c);
+// console.log(a!==b);
+
+// var a = '5';
+// var b = '5';
+// console.log(a!==b);
+
+// console.log(0==false);
+// console.log(1==true);
+
