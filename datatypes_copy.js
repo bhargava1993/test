e52f7763cb8a103390582e57b320421cd08e778b
unknown <1993bhargava@gmail.com>
2025-11-18 09:13:29 +0530

    array methods basics

---
+++ datatypes_copy.js
@@ -0,0 +1,191 @@
+// HTML,
+// css,
+
+// UI/uX ---> pagecode into html css templates
+// javascript --> heart
+
+// library -->
+
+// React.js -- library
+
+// --------------------------------
+// Node.js --> runtime environment
+// mogodb
+
+// salray --> equal
+
+
+// AbortController, 123, !!+, ./
+// a-65
+// binary language --> 010101
+
+
+// v8-engine
+
+// C++
+
+
+// datatypes:
+// ------------
+// data - type
+
+// abcs - string
+// 1234 - number
+
+// 12  -  intger, real
+// 12.10  - deciamal , floating
+// 10.5
+// .75
+// 1000.1
+
+// +-/@#$ - symbol
+
+// bigInt 
+
+// 2 types of datatypes:
+// -------------------------
+// 1.premitives
+
+// you can able to store single values
+// string
+// number
+// symbal
+// null
+// undifind
+// bigInt
+// Boolen - true, false (0, 1)
+
+// nan - not a number
+// let firstname = "bhargava reddy I am working in nttdata";
+
+
+// console.log(typeof firstname);
+// let salary = 10000;
+// console.log(typeof salary);
+// let balanceInbank = 1000.75;
+// console.log(typeof balanceInbank);
+// let success = true;
+// console.log(typeof success);
+// let failure = false;
+// console.log(typeof failure);
+
+// let empId=null;
+// console.log(empId);
+
+
+// 2. non-premetives
+// arrays
+// objects
+// function --> reuse code
+
+// let myfrineds = "venu, sudher, zaiba, ramu";
+// ["",1,2,3,4,null, undefined, true, false];
+
+// let myfrineds = ["venu", "sudher", "zaiba", "ramu", true, 123, null,""];
+//                     // 0       1           2       3  
+// console.log(myfrineds[0]);
+// console.log(myfrineds[1]);
+// console.log(myfrineds[2]);
+// console.log(myfrineds[3]);
+// console.log(myfrineds);
+
+let classMembers= ["venu", "sudher", "zaiba", "ramu", 590, 580, 600, 200];
+
+// Object
+// { key : value}
+
+// let familymembers= ["bhargava","raja","tulasi","purandhar","dhanvika"];
+
+let bhargavamembers={
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+}
+
+let sudhermembers={
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+}
+
+let venumembers={
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+}
+
+let zaibamembers={
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+}
+
+let apartment=[{
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+},{
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "children":["dhanvika","bhargava","sita","shruthi",
+         {
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+}]
+},{
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+},{
+    "fathername":"raja",
+    "mothername":"tulasi",
+    "elderbrother":"purandhar",
+    "myself":"bhargava",
+    "daughter":"dhanvika"
+}]
+
+console.log(typeof zaibamembers);
+
+// [{name: "venu",
+// totalMarks: 590},
+
+// {name: "sudher",
+// totalMarks: 580},
+
+// {name: "zaiba",
+// totalMarks: 580},
+
+// {name: "ramu",
+// totalMarks: 200}]
+                      
+
+
+className - camel case
+familyMember
+
+employedetails
+
+employee_details
+EmployeeDetails - pascal case
+
+
+
+
+
--- loops.js
+++ loops.js
@@ -37,9 +37,6 @@
 // 0+1 =1
 
 
-
-
-
 // console.log(0)
 // console.log(1)
 // console.log(2)
@@ -127,6 +124,154 @@
 // 121
 
 
-1 - 100
-100 - 1
+// 1 - 100
+// 100 - 1
 
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+// 54321
+// 5432
+// 543
+// 54
+// 5
+
+// let studentNames=[
+//   "Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava", "Elijah", "Sophia", "William", "Isabella",
+//   "James", "Mia", "Benjamin", "Charlotte", "Lucas", "Amelia", "Henry", "Harper", "Alexander", "Evelyn",
+//   "Michael", "Abigail", "Ethan", "Ella", "Daniel", "Avery", "Jacob", "Scarlett", "Logan", "Grace",
+//   "Jackson", "Chloe", "Levi", "Victoria", "Sebastian", "Riley", "Mateo", "Aria", "Jack", "Lily",
+//   "Owen", "Hannah", "Theodore", "Layla", "Aiden", "Zoey", "Samuel", "Nora", "Joseph", "Mila",
+//   "John", "Aubrey", "David", "Camila", "Wyatt", "Penelope", "Matthew", "Luna", "Luke", "Ella",
+//   "Asher", "Stella", "Carter", "Paisley", "Julian", "Savannah", "Grayson", "Skylar", "Leo", "Nova",
+//   "Jayden", "Isla", "Gabriel", "Ellie", "Isaac", "Hazel", "Lincoln", "Violet", "Anthony", "Aurora",
+//   "Hudson", "Genesis", "Dylan", "Emilia", "Ezra", "Everly", "Thomas", "Leah", "Charles", "Madison",
+//   "Christopher", "Willow", "Jaxon", "Lucy", "Maverick", "Brooklyn", "Josiah", "Claire"
+// ]
+
+// console.log(studentNames.length);
+
+// for(let i=0;i<studentNames.length;i++){
+//     console.log(studentNames[i]);
+// }
+
+ 
+// console.log(studentNames.length);
+
+// for(let i=0;i<studentNames.length;i++){
+//     console.log(studentNames[i])
+//     // console.log(i);
+// }
+
+//length -- length of an array
+//push -- is going to add one new element at the end
+//pop -- is going to remove one element at the end
+//shift -- is going to remove one element from staring of an array
+//unshift -- is going to add one element from staring of an array
+
+// "Oliver", "Ava", "Elijah", "Sophia", "William", "Isabella"
+
+// let studentNames=["Liam", "Olivia", "Noah", "Emma"];
+
+// console.log("old one---",studentNames.length);
+
+// studentNames.push("Oliver");
+// studentNames.push("Ava");
+// studentNames.push("Elijah");
+// studentNames.push("Oliver");
+// console.log("new one---",studentNames);
+
+
+// let studentNames=["Liam", "Olivia", "Noah", "Emma"];
+
+// console.log("old one---",studentNames);
+
+// studentNames.pop();
+// studentNames.pop();
+// studentNames.pop();
+// studentNames.pop();
+// console.log("new one---",studentNames)
+
+// let studentNames=["Liam", "Olivia", "Noah", "Emma"];
+
+// console.log("old one---",studentNames);
+
+// studentNames.shift();
+// studentNames.shift();
+// studentNames.shift();
+// console.log("new one---",studentNames)
+
+// let studentNames=["Liam", "Olivia", "Noah", "Emma"];
+
+// console.log("old one---",studentNames);
+
+// studentNames.unshift("123");
+// studentNames.unshift("bhargava");
+// console.log("new one---",studentNames)
+
+
+// replacing a value
+// let studentNames=["Liam", "Olivia", "Noah", "Emma"];
+
+// console.log(studentNames[2]);
+
+// console.log("old one---",studentNames);
+
+// studentNames[3] = "venu";
+
+// console.log("new one---",studentNames)
+
+// let firstClass=["Liam", "Olivia", "Noah", "Emma"];
+
+// let secondClass=["Ava", "Elijah", "Sophia", "William", "Isabella"];
+
+// let thiredClass=[]
+
+// console.log("old one---",thiredClass)
+
+// Class3rd.push(studentNames1st[0]);
+// Class3rd.push(studentNames1st[1]);
+// Class3rd.push(studentNames1st[2]);
+// Class3rd.push(studentNames1st[3]);
+
+// for(let i=0; i< firstClass.length;i++){
+
+//     thiredClass.push(firstClass[i]);
+// }
+
+// Class3rd.push(studentNames2nd[0]);
+// Class3rd.push(studentNames2nd[1]);
+// Class3rd.push(studentNames2nd[2]);
+// Class3rd.push(studentNames2nd[3]);
+// Class3rd.push(studentNames2nd[4]);
+
+// for(let i=0; i< secondClass.length;i++){
+
+//     thiredClass.push(secondClass[i]);
+// }
+
+// console.log("new one---",thiredClass)
+
+
+
+let firstClass=[ "Liam", "Olivia",  "venu", "Noah", "Emma",];
+
+// console.log(firstClass[2]);
+
+// console.log(firstClass[4]  === "venu");
+
+// console.log("old one--------",firstClass);
+
+// for(let i=0; i< firstClass.length;i++){
+
+//     if(firstClass[i]  === "venu"){
+//         // console.log("venu avaliable");
+//         firstClass[i] = "sudher";
+//     }
+
+// }
+
+// console.log("old one--------",firstClass);
