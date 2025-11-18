e676487f76963a62bd43f3753aac5b6c2cb9bded
Venu <muddangulavenukumar@gmail.com>
2025-11-18 11:46:26 +0530

    Basic methods of Array

---
+++ Methods_of_Arrays.js
@@ -0,0 +1,150 @@
+// Array Methods
+// 1. length -----> To find how many values in an array
+// 2. push -----> To add one element at the end of an array
+// 3. pop ------> To remove one element at the end of an array
+// 4. shift -----> To remove one element at the starting of an array
+// 5. unshift ----> To add one element at the starting of an array
+
+
+// let fruits = ["Apple", "Banana", "Grapes", "Mango", "Orange", "Straw_berry"];
+
+// console.log(fruits.length);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[5]);
+
+// fruits.push("Cherry");
+// console.log(fruits);
+
+// fruits.push("Guava", "Water_melon", "Pine_aple");
+// console.log(fruits);
+// console.log(fruits.length);
+
+
+
+// fruits.pop();
+// fruits.pop();
+// console.log(fruits);
+
+
+
+// fruits.pop();
+// console.log(fruits.length);
+
+
+// fruits.shift();
+// fruits.shift();
+// console.log(fruits);
+// console.log(fruits.length);
+
+
+
+// fruits.unshift()
+// console.log(fruits);
+// console.log(fruits.length);
+
+
+// fruits.unshift("Pomogranite", "Sapota");
+// console.log(fruits);
+// console.log(fruits.length);
+
+
+//  Replace  a fruit into another fruit using index positions
+// let fruits = ["Apple", "Banana", "Grapes", "Mango", "Orange", "Straw_berry"];
+
+// fruits[3] = "Sapota";
+// console.log(fruits);
+// fruits[5] = "PineApple";
+// console.log(fruits);
+// fruits[1] = "Jack_Fruit";
+// console.log(fruits);
+
+// using for loop to print the array values
+// for (i = 0; i <= 5; i += 1) {
+//     console.log(fruits[i]);
+// }
+
+
+// to print the array values in reverse order
+// for (i = 5; i > 0; i -= 1) {
+//     console.log(fruits[i]);
+// }
+
+
+//  To check an array has Grapes or not using for loop
+
+// let fruits = ["Apple", "Banana", "Grapes", "Mango", "Orange", "Straw_berry"];
+
+// for (i = 0; i < fruits.length; i += 1) {
+//     if (fruits[i] === "Grapes") {
+//         console.log("Grapes are available in this array");
+//         // } else {
+//         //     console.log("Grapes are not available in this array");
+//     }
+// }
+
+
+//  To check an array has Banana or not using for loop 
+
+// for (i = 0; i < fruits.length; i += 1) {
+//     if (fruits[i] === "Banana") {
+//         console.log("Banana is available");
+//     }
+// }
+
+
+
+//  TO replace a fruit into an array using for loop
+
+// let fruits = ["Apple", "Banana", "Grapes", "Mango", "Orange", "Straw_berry"];
+
+
+// for (i = 0; i < fruits.length; i += 1) {
+//     if (fruits[i] === "Orange") {
+//         console.log("Orange is available");
+//         fruits[i] = "Pine apple";
+//     }
+// }
+
+// console.log(fruits);
+
+
+
+//  Replace a particular value using for loop
+
+let animals = ["Tiger", "Lion", "Deer", "Elephant", "Dog", "Cat", "Zirafee", "Zebra", "Monkey"];
+
+// for (i = 0; i < animals.length; i += 1) {
+//     if (animals[i] === "Zirafee") {
+//         console.log("Zirafee is available in this array");
+//         animals[i] = "Donkey";
+//     }
+// }
+// console.log(animals);
+
+// for (i = 0; i < animals.length; i += 1) {
+//     if (animals[i] === "Deer") {
+//         console.log("Deer is availble");
+//         animals.push("Camel");
+//     }
+// }
+// console.log(animals);
+
+
+// for (i = 0; i < animals.length; i += 1) {
+//     if (animals[i] === "Deer") {
+//         console.log("Deer is available");
+//         animals.pop();
+//     }
+// }
+// console.log(animals);
+
+
+
+
+
+
+
+
+
+
