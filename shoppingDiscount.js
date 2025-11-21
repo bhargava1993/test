8953df9737c944b96ed01661df8d3502b121fae4
Sudheerkumar <balagundlanswamy123@gmail.com>
2025-11-21 16:03:22 +0530

    Nested If

---
+++ shoppingDiscount.js
@@ -0,0 +1,89 @@
+/*
+
+let amount = 70;
+
+if (amount>50){
+    if (amount>=500){
+        console.log("You will get 50% Discount");
+    }else if(amount>=100){
+        console.log("You will get 25% Discount");
+    }else{
+        console.log("You will get 10% Discount");
+    }
+}else{
+    console.log("You will not get any Discount");
+}
+
+# OUTPUT
+
+You will get 10% Discount
+
+*/
+
+
+/*
+
+let amount = 1500;
+
+if (amount>50){
+    if (amount>=500){
+        console.log("You will get 50% discount");
+    }else if(amount>=100){
+        console.log("You will get 25% discount");
+    }else {
+        console.log("You will get 10% discount");
+    }
+}else{
+    console.log("You will not get any discount");
+}
+
+# OUTPUT
+
+You will get 50% discount
+
+*/
+
+
+
+/*
+let amount = 24;
+
+if (amount>50){
+    if (amount>=500){
+        console.log("You will get 50% discount");
+    }else if(amount>=100){
+        console.log("You will get 25% discount");
+    }else{
+        console.log("You will get 10% discount");
+    }
+}else{
+    console.log("You will not get any discount");
+}
+
+# OUTPUT
+
+You will not get any discount
+
+*/
+
+
+/* 
+
+let amount = 123;
+
+if (amount>50){
+    if (amount>=500){
+        console.log("You wil get 50% discount");
+    }else if(amount>=100){
+        console.log("You wil get 20% discount");
+    }else {
+        console.log("You will get 10% discount");
+    }
+}else{
+    console.log("You will not get any discount");
+}
+
+# OUTPUT
+
+You wil get 20% discount
+*/
---
+++ temperatureCheck.js
@@ -0,0 +1,55 @@
+/*
+
+let temperature = 36;
+
+if (temperature>=0){
+    if (temperature<=30){
+        console.log("Temperature is a Normal");
+    }
+    else{
+        console.log("Temperature is HOT");
+    }
+}else{
+    console.log("It's freezing....");
+}
+
+# OUTPUT
+Temperature is HOT
+
+*/
+
+/*
+let temperature = -53;
+
+if (temperature>=0){
+    if (temperature<=30){
+        console.log("Temperature is Normal");
+    }else{
+        console.log("Temperature is Hot");
+    }
+}else{
+    console.log("It's Freezing");
+}
+
+# OUTPUT
+It's Freezing
+*/
+
+
+/*
+let temperature = 23;
+
+if (temperature>=0){
+    if (temperature<=30){
+        console.log("Temperature is Normal");
+    }else{
+        console.log("Temperature is Hot");
+    }
+}else {
+    console.log("It's freezing");
+}
+
+# OUTPUT
+
+Temperature is Normal
+*/
