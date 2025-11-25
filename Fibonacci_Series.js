35695b6e982ec6d5baa8507cb0046c2753fb910f
Venu <muddangulavenukumar@gmail.com>
2025-11-25 11:33:13 +0530

    Fibonacci series program

--- Fibonacci_Series.js
+++ Fibonacci_Series.js
@@ -1,4 +1,4 @@
-// let num = 10;
+// let num = 10;         // Prints 10 numbers
 // let a = 0;
 // let b = 1;
 
@@ -12,14 +12,36 @@
 //     b = c;
 // }
 
-let num = 7;
+
+
+//                           (or)
+
+
+
+// let num = 10;
+// let a = -1;
+// let b = 1;
+
+
+// for (let i = 0; i <= num; i++) {
+//     let c = a + b;
+//     console.log(c);
+//     a = b;
+//     b = c;
+// }
+
+
+
+
+let num = 10;          // Prints numbers below 10
 let a = -1;
 let b = 1;
 
-
 for (let i = 0; i <= num; i++) {
     let c = a + b;
-    console.log(c);
-    a = b;
-    b = c;
+    if (c <= num) {
+        console.log(c);
+        a = b;
+        b = c;
+    }
 }
