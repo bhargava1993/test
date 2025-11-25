403b4c7e056892032cc8c1053ae417e9b3effc66
Venu <muddangulavenukumar@gmail.com>
2025-11-25 11:26:37 +0530

    Practice Array methods

--- Methods_of_Arrays.js
+++ Methods_of_Arrays.js
@@ -4,6 +4,27 @@
 // 3. pop ------> To remove one element at the end of an array
 // 4. shift -----> To remove one element at the starting of an array
 // 5. unshift ----> To add one element at the starting of an array
+// 6. indexOf ------> It uses the find the index number of element in an array
+//                    If the element is found it prints the index number of an element else it will print -1
+//
+// 7.includes -------> It uses  wheather the element is exist or not
+//                     If the element is exist it prints true else false
+
+// 8. slice ------> It is used to copy some part of an array
+//                  It doesn't changes the original array
+//                  It returns the new array
+// syntax : array.slice(start,end)-----> end index is not included
+//          arra.slice(2,6)
+//
+// 9.splice -------> It is used to add,remove and replace the elements of an array
+//                   It changes the original array
+//                   It returns the removed elements of an array
+// syntax : array.splice(startIndex, deleteCount, item_1,item_2.....)
+//
+// 10.reverse ---- > It is used to reverse an array
+//                   It changes the original array
+//
+
 
 
 // let fruits = ["Apple", "Banana", "Grapes", "Mango", "Orange", "Straw_berry"];
@@ -84,7 +105,7 @@
 // }
 
 
-//  To check an array has Banana or not using for loop 
+//  To check an array has Banana or not using for loop
 
 // for (i = 0; i < fruits.length; i += 1) {
 //     if (fruits[i] === "Banana") {
@@ -112,7 +133,7 @@
 
 //  Replace a particular value using for loop
 
-let animals = ["Tiger", "Lion", "Deer", "Elephant", "Dog", "Cat", "Zirafee", "Zebra", "Monkey"];
+// let animals = ["Tiger", "Lion", "Deer", "Elephant", "Dog", "Cat", "Zirafee", "Zebra", "Monkey"];
 
 // for (i = 0; i < animals.length; i += 1) {
 //     if (animals[i] === "Zirafee") {
@@ -141,10 +162,97 @@ let animals = ["Tiger", "Lion", "Deer", "Elephant", "Dog", "Cat", "Zirafee", "Ze
 
 
 
+// let animals = ["Tiger", "Lion", "Deer", "Elephant", "Dog", "Cat", "Zirafee"];
+
+// animals.push("Monkey", "Donkey");
+// console.log(animals);
+
+// console.log(animals.length);
+
+
+// // animals.pop();
+// animals.pop("Cat");   // we can't remove particular element of an array by using pop
+// console.log(animals);
+
+
+// animals.shift();
+// console.log(animals);
+
+
+// animals.unshift("Bear");
+// console.log(animals);
+
+// let animals = ["Tiger", "Lion", "Deer", "Elephant", "Dog", "Cat", "Zirafee"];
+
+// console.log(animals.indexOf("Cat"));
+// console.log(animals.indexOf("Lion"));
+// // console.log(animals)
+// console.log(animals.includes("Tiger"));
+// console.log(animals.includes("Elephant"));
+// console.log(animals.includes("Bear"));
+
+// console.log(animals.slice(3));   // [ 'Elephant', 'Dog', 'Cat', 'Zirafee' ]
+// console.log(animals);
+// ----output----
+// [
+//   'Tiger',   'Lion',
+//   'Deer',    'Elephant',
+//   'Dog',     'Cat',
+//   'Zirafee'
+// ]
+
+// console.log(animals.slice(2, 5));
+// console.log(animals);
+
+// console.log(animals.slice(3, 6));
+// console.log(animals);
+
+
+// let animals = ["Tiger", "Lion", "Deer", "Elephant", "Dog", "Cat", "Zirafee"];
+
+// animals.splice(2, 3);
+// console.log(animals);   // [ 'Tiger', 'Lion', 'Cat', 'Zirafee' ]
+
+
+// let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// numbers.splice(2, 3);
+// console.log(numbers);   //[ 10, 20, 60, 70, 80, 90 ]
 
 
 
+// numbers.splice(3, 6, "Venu", "Jagan");
+// console.log(numbers);
 
+
+// numbers.splice(2, 4, "Apple", "Banana", "Orange");
+// console.log(numbers);
+
+
+//  Replacing a value
+
+// numbers.splice(5, 1, "Car");
+// console.log(numbers);
+
+
+// numbers.splice(2, 1, "Apple");
+// console.log(numbers);
+
+
+// numbers.splice(7, 2, "Hyderabad", "Bengaluru");
+// console.log(numbers);
+
+
+// let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+// console.log(numbers.reverse());
+
+// reverese = [];
+
+// for (let i = numbers.length - 1; i >= 0; i--) {
+
+//     reverese.push(numbers[i]);
+// }
+// console.log(reverese);
 
 
 
