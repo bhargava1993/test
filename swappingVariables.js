f019001ef94f7f672474e3b39f0d351f9aedeb31
Venu <muddangulavenukumar@gmail.com>
2025-12-03 22:03:40 +0530

    swapping of two variables

---
+++ swappingVariables.js
@@ -0,0 +1,21 @@
+// let a = [10, 20, 30, 40, 50];
+// let b = [60, 70, 80, 90, 100];
+
+
+// let copy = a;
+// a = b;
+// b = copy;
+
+// console.log(a);
+// console.log(b);
+
+
+let a = 5;
+let b = 10;
+
+let copy = a;
+a = b;
+b = copy;
+
+console.log(a);
+console.log(b);
