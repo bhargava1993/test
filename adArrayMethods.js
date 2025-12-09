b9eaa5cbaaaf9d4048f0300c6bfe75e46efd28f6
Venu <muddangulavenukumar@gmail.com>
2025-12-09 19:03:16 +0530

    Practising advanced array methods

--- adArrayMethods.js
+++ adArrayMethods.js
@@ -1,4 +1,4 @@
-let array = ["Apple", "Banana", "Grapes", "Pineaplle"];
+// let array = ["Apple", "Banana", "Grapes", "Pineaplle"];
 
 // array.forEach((ele) => {
 //     console.log(ele);
@@ -52,7 +52,7 @@ let array = ["Apple", "Banana", "Grapes", "Pineaplle"];
 
 
 
-array = [10, 20, 30, 40];
+// array = [10, 20, 30, 40];
 
 // array.filter((ele, index, arr) => {
 //     console.log(ele, "----", index, arr);
@@ -70,3 +70,147 @@ array = [10, 20, 30, 40];
 // });
 // console.log(num);
 
+
+
+
+
+
+
+
+
+
+// reduce
+
+// const todos = [
+//     { id: 1, text: "Learn JS", done: true },
+//     { id: 2, text: "Learn React", done: false },
+//     { id: 3, text: "Learn Redux", done: true },
+//     { id: 4, text: "Learn Node.js", done: false }
+// ];
+
+// todos.reduce((accumulator, currentValue, index, array) => {
+//     // console.log("accu ----------", accumulator);
+//     // console.log("curr --------------", currentValue);
+//     // console.log("index -------", index);
+//     console.log("array -----", array);
+
+// }, 2);
+// let completed = {};
+// let pending = {};
+
+// todos.reduce((accumulator, currentValue, index, array) => {
+//     if (currentValue.done === true) {
+//         obj['completed'].push(currentValue);
+
+//     } else {
+//         obj["pending"].push(currentValue);
+//     }
+
+// }, 2);
+
+
+
+// const todos = [
+//     { id: 1, text: "Learn JS", done: true },
+//     { id: 2, text: "Learn React", done: false },
+//     { id: 3, text: "Learn Redux", done: true },
+//     { id: 4, text: "Learn Node.js", done: false }
+// ];
+
+// let result = todos.reduce((acc, currtValue) => {
+//     const key = currtValue.done ? "completed" : "pending";
+//     acc[key].push(currtValue);
+//     return acc;
+
+// }, { "completed": [], "pending": [] })
+
+// console.log("result---------", result);
+
+
+// let result = todos.reduce((acc, currValue) => {
+//     // const key = currValue.done ? "completed" : "pending";
+//     if (currValue.done === true) {
+//         acc["completed"].push(currValue);
+//     } else {
+//         acc["pending"].push(currValue);
+//     }
+//     return acc;
+// }, { "completed": [], "pending": [] })
+
+// console.log(result);
+
+
+
+// let array = [10, 39, 47, 29, 34, 13, 44, 2];
+
+
+// let result = array.reduce((acc, currValue) => {
+//     return acc;
+// }, 0)
+// console.log(result);
+
+// let result = array.reduce((acc, curr) => {
+//     return (acc > curr) ? acc : curr
+// }, 0)
+// console.log(result);
+
+
+// let result = array.reduce((acc, curr) => {
+//     if (curr > acc) {
+//         return curr;
+//     }
+//     return acc;
+// }, 0)
+// console.log(result);
+
+
+// let result = array.reduce((acc, curr) => {
+//     if (curr < acc) {
+//         return curr;
+//     }
+//     return acc;
+// });
+// console.log(result);
+
+
+
+let array = [10, 39, 47, 29, 34, 13, 44, 2];
+
+
+// let fruits = [
+//     { id: 1, "apple": "Red" },
+//     { id: 2, "banana": "Yellow" }
+// ]
+
+// let result = fruits.find((ele) => {
+//     return ele.id < 2;
+// });
+// console.log(result);
+
+// let result = array.find((ele) => {
+//     return ele > 2;
+// });
+// console.log(result);
+
+
+// let result = array.findIndex((ele) => {
+//     return ele === 2;
+// });
+// console.log(result);
+
+
+// let result = array.some((ele) => {
+//     return ele > 4;
+// })
+// console.log(result);
+
+// let result = array.every((ele) => {
+//     return ele > 1;
+// });
+// console.log(result);
+
+
+let result = array.every((ele) => {
+    return ele > 10;
+});
+console.log(result);
