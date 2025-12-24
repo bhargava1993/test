d5965d6539e7084ff5812ed86190a4129a33f6be
Venu <muddangulavenukumar@gmail.com>
2025-12-24 18:04:44 +0530

    practicing forEach,map,filter,reduce array methods

--- adArrayMethods.js
+++ adArrayMethods.js
@@ -117,6 +117,8 @@
 //     { id: 4, text: "Learn Node.js", done: false }
 // ];
 
+
+
 // let result = todos.reduce((acc, currtValue) => {
 //     const key = currtValue.done ? "completed" : "pending";
 //     acc[key].push(currtValue);
@@ -174,7 +176,7 @@
 
 
 
-let array = [10, 39, 47, 29, 34, 13, 44, 2];
+// let array = [10, 39, 47, 29, 34, 13, 44, 2];
 
 
 // let fruits = [
@@ -210,7 +212,186 @@ let array = [10, 39, 47, 29, 34, 13, 44, 2];
 // console.log(result);
 
 
-let result = array.every((ele) => {
-    return ele > 10;
-});
-console.log(result);
+// let result = array.every((ele) => {
+//     return ele > 10;
+// });
+// console.log(result);
+
+
+let array=["Apple","Banana","Carrot","Grapes","Sapota","jackFruit"];
+
+// array.forEach((ele)=>{
+//     console.log(ele);
+// });
+
+
+// array.map((ele)=>{
+//     console.log(ele);
+// })
+
+// array.filter((ele)=>{
+//     console.log(ele)
+// })
+
+
+// let num=[10,20,30,40];
+// array.forEach((ele,index,arr)=>{
+//     console.log(ele+10)
+
+
+// })
+
+// var a=[];
+// num.forEach(num=>{
+//     // console.log(num+10)
+//     // let a=[]
+//     a.push(num*10)
+// })
+// console.log(a);
+
+
+
+
+// num.forEach(nums=>{
+//     console.log(nums*2);
+// })
+
+
+// array.forEach((ele,index,arr)=>{
+//     console.log(arr)
+
+// })
+
+
+
+
+// let num=[10,20,30,40];
+// let a=[];
+
+// num.forEach(ele=>{
+//     a.push(ele*4);
+// })
+// console.log(a);
+
+// let b=[];
+
+// num.forEach(ele=>{
+//     b.push(ele/2);
+// })
+// console.log(b);
+
+
+
+
+// let fruits=["Apple","Orange","Pine apple"];
+//  let result= fruits.map((ele)=>{
+//     return ele.toLowerCase(ele)
+//  });
+//  console.log(result);
+
+//  let result= fruits.map((ele)=>{
+//     return ele.toUpperCase(ele)
+//  });
+//  console.log(result);
+
+
+// fruits.map((ele)=>{
+//     console.log(ele);
+// })
+
+// fruits.map(ele=>{
+//     console.log(ele);
+// });
+
+
+// let a=[110,20,12,23,35,23,72];
+// let b=[];
+
+// a.forEach( ele=>{
+//     if (ele%2===0){
+//         b.push(ele)   
+//     }
+// })
+// console.log(b);
+
+
+// let result=a.map(ele=>{
+//     console.log(ele);
+// })
+// console.log(result);
+// console.log(a);
+
+// let result=a.map((ele)=>{
+//     return ele
+// })
+// console.log(result);
+
+
+// let a=[110,20,12,23,35,23,72];
+
+
+// let result=a.filter(ele=>{
+//     return ele%2!=0;
+// })
+// console.log(result);
+
+// let result=a.filter(ele=>{
+//     return ele<=30;
+// })
+// console.log(result);
+
+// let result= a.map((ele)=>{
+//     return ele/2;
+// })
+// console.log(result);
+
+// let result=a.filter((ele,index)=>{
+//     return ele===35;
+// })
+// console.log(result);
+
+
+// let a=[110,20,12,23,35,23,72];
+
+// let result= a.reduce((acc,currValue,index,arr)=>{
+//     return acc=acc+currValue;
+// },40);
+// console.log(result);
+
+// let a=[10]
+
+// let a=[110,20,12,23,35,23,72];
+// let result=a.reduce((acc,currValue)=>{
+//     return acc=(acc+currValue)*2;
+// },2)
+// console.log(result);
+
+
+let a=[110,20,12,23,35,23,72];
+
+// let result=a.find((ele)=>{
+//     return ele===72;
+// })
+// console.log(result);
+
+// let result=a.findIndex((ele)=>{
+//     return ele===53;
+// })
+// console.log(result);
+
+
+// let result=a.some((ele)=>{
+//     return ele>53;
+// })
+// console.log(result);
+
+// let result=a.some((ele)=>{
+//     return ele>27;
+// })
+// console.log(result);
+
+
+// let result=a.every((ele)=>{
+//     return ele>10;
+// })
+// console.log(result);
